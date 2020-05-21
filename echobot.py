ad5087b0b9abf04807aa6323006da93e3688d206
Alessandro Migliardi <ale.migliardi@gmail.com>
2020-05-21 20:19:28 +0200

    Added config file, token is loaded from config

--- echobot.py
+++ echobot.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# This program is dedicated to the public domain under the CC0 license.
+# This program is dedicated to the public domain under the CC0 license
 
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
+import json
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
@@ -51,10 +39,13 @@ def error(update, context):
 
 def main():
     """Start the bot."""
+    # Load config
+    with open("config.json") as f:
+        config = json.load(f)
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("TOKEN", use_context=True)
+    updater = Updater(config["TOKEN"], use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
