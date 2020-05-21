ed4dd4fc1dfae21ee7db03a0082523c6dad9ee00
Alessandro Migliardi <ale.migliardi@gmail.com>
2020-05-21 20:48:30 +0200

    Formatting did not work?

--- echobot.py
+++ echobot.py
@@ -37,7 +37,7 @@ def fact(update, context):
     try:
         r = requests.get("https://uselessfacts.jsph.pl/random.json", timeout=5)
         data = r.json()
-        update.message.reply_text(f"_{data['text']}_ ([{data['source']}]({data['source_url']}))")
+        update.message.reply_text(f"*{data['text']}* [{data['source']}]({data['source_url']})")
     except:
         update.message.reply_text("Could not fetch a random fact")
 
