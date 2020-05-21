4d31887c301fbe67131b6b8c48f266fc3658df70
Alessandro Migliardi <ale.migliardi@gmail.com>
2020-05-21 20:33:43 +0200

    oops forgot to register

--- echobot.py
+++ echobot.py
@@ -68,6 +68,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("fact", fact))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
