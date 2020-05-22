ffe8e32c2e1fd802eb0037ad6625b5b267d93836
Erbrow1 <stefano@Stefano.localdomain>
2020-05-22 14:39:10 +0200

    Rimesso tutto a posto

--- echobot.py
+++ echobot.py
@@ -9,7 +9,7 @@ import requests
 from uuid import uuid4
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-all_user_data=dict()
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -56,41 +56,6 @@ def classes(update,context):
 
     update.message.reply_text(message)
 
-
-def put(update, context):
-    """Usage: /put value"""
-    # Generate ID and seperate value from command
-    key = str(uuid4())
-    value = update.message.text.partition(' ')[2]
-
-    user_id = update.message.from_user.id
-
-    # Create user dict if it doesn't exist
-    if user_id not in all_user_data:
-        all_user_data[user_id] = dict()
-
-    # Store value
-    user_data = all_user_data[user_id]
-    user_data[key] = value
-
-    update.message.reply_text(key)
-
-def get(update, context):
-    """Usage: /get uuid"""
-    # Seperate ID from command
-    key = update.message.text.partition(' ')[2]
-
-    user_id = update.message.from_user.id
-
-    # Load value
-    try:
-        user_data = all_user_data[user_id]
-        value = user_data[key]
-        update.message.reply_text(value)
-
-    except KeyError:
-        update.message.reply_text('Not found')
-               
     
 def error(update, context):
     """Log Errors caused by Updates."""
