0b2879dcbd34fc096ec3d0d9ec5910f730922033
Erbrow1 <stefano@Stefano.localdomain>
2020-05-22 02:44:20 +0200

    resolve print Please enter the commit message for your changes. Lines starting

--- echobot.py
+++ echobot.py
@@ -47,9 +47,9 @@ def fact(update, context):
 def classes(update,context):
     """Gets list of classes"""
     with open(classes) as fc:
-        data = fx.read().split("\n")
-        for elem in data:
-            print(elem)
+        data = fc.read().split("\n")
+    for elem in data:
+        update.message.reply_text(elem)
         
         
     
