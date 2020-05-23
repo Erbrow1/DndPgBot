1fb5c35ee7963fc8119e75c0675f8cfcb64d2167
Erbrow1 <stefano@Stefano.localdomain>
2020-05-23 21:37:50 +0200

    Aggiunto default roll

--- dndbot.py
+++ dndbot.py
@@ -51,7 +51,9 @@ def help(update, context):
     update.message.reply_text('Lista comandi:\n/help (mostra questa lista)\n/me (informazioni utente)\n/makepg \"NomePg\" (Crea nuovo personaggio)\n/roll \"Number\" (estrae un numero casuale tra 1 e Number)')
 
 def roll(update,context):
-    num= random.randint(1,int(context.args[0]))
+    if context.args[0] != None :
+        num= random.randint(1,int(context.args[0]))
+    else num= random.randint(1,20)
     update.message.reply_text(f"You rolled {num}") 
 
 def button(update, context):
