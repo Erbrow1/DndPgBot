e476ff1e56642b6c72d710b7a18e7f8b4f5dbd2c
Erbrow1 <stefano@Stefano.localdomain>
2020-05-23 16:54:32 +0200

    tolto veramente echo

--- dndbot.py
+++ dndbot.py
@@ -114,8 +114,6 @@ def main():
     dp.add_handler(CommandHandler("interactive",interactive))
     dp.add_handler(CommandHandler("stop_interactive",stop_interactive))
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
